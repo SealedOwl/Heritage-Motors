5ef31834e738150d4fe62af53beac2192ed16815
SealedOwl <sealedowl@gmail.com>
2025-11-18 20:22:03 +0530

    style: add mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 				</div>
 
 				{/*desktop nav links  */}
-				<div className="hidden md:flex items-center text-gold text-xl gap-5">
+				<div className="hidden md:flex items-center text-gold text-xl gap-5 xl:gap-10">
 					<a href="#" className="hover:text-white transition">
 						Home
 					</a>
@@ -56,6 +56,86 @@ const Navbar = () => {
 			</div>
 
 			{/* mobile slide-out menu  */}
+			<div
+				className={`bg-charcoal fixed top-0 right-0 h-full w-60 border shadow-lg transform transition-all duration-300 ${
+					openMenu ? "translate-x-0" : "translate-x-full"
+				}`}
+			>
+				<div className="flex items-center justify-between p-5">
+					<p className="text-xl text-gold font-playfair">Menu</p>
+					<button
+						className="px-3 py-1 border border-gold text-gold rounded "
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						X
+					</button>
+				</div>
+
+				{/* mobile nav links  */}
+				<div className="flex flex-col text-gold text-xl gap-5 p-5">
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						Home
+					</a>
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						Inventory
+					</a>
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						Collecttions
+					</a>
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						Services
+					</a>
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						About Us
+					</a>
+					<a
+						href="/"
+						onClick={() => {
+							setOpenMenu((prev) => !prev);
+						}}
+					>
+						Contact
+					</a>
+				</div>
+				{/* search  */}
+				<div className="flex flex-col p-5 gap-2">
+					<input
+						type="text"
+						placeholder="search..."
+						className="w-full rounded bg-white px-2 "
+					/>
+					<button className="text-gold border border-gold py-1 px-2 rounded">
+						Search
+					</button>
+				</div>
+			</div>
 		</nav>
 	);
 };
